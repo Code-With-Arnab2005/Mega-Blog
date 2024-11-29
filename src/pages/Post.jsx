19161e38dd79409a014218d38749d166a4d03394
Arnab Bhattacharya <arnabbhattacharya2005@gmail.com>
2024-11-29 10:31:04 +0530

    add set as acitve functionaliey

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -10,6 +10,7 @@ function Post() {
 
     const [post, setPost] = useState(null);
     const userData = useSelector((state) => state.auth.userData);
+    const [isActive, setIsActive] = useState(null);
     const navigate = useNavigate();
     const [imgurl, setImgurl] = useState("");
 
@@ -20,14 +21,10 @@ function Post() {
             service.getPost(slug).then((post) => {
                 if (post) {
                     setPost(post);
-                    // console.log("post: ", post);
-                    // console.log("featured img: ", post.featuredImage);
+                    setIsActive(post.status)
                     service.getFilePreview(post.featuredImage).then((url) => {
-                        // console.log("url: ", url)
                         setImgurl(url);
                     })
-                    // console.log("featured image: ", post.featuredImage)
-                    // console.log("image url: ", imgurl)
                 }
                 else navigate("/")
             })
@@ -36,11 +33,6 @@ function Post() {
 
 
     const isAuthor = post && userData ? post.userId === userData.user.$id : false;
-
-    // console.log("post: ", post);
-    // console.log("postid: ", post.userId);
-    // console.log("userdata id: ", userData.user.$id);
-
 
     const deletePost = async () => {
         if (slug) {
@@ -51,7 +43,20 @@ function Post() {
             }
         }
     }
-    // console.log("isauthor: ", isAuthor);
+
+    const handleActive = async () => {
+        if(isActive==='active' && post){
+            const updatedPost = await service.updatePost(slug, {...post, status: "inactive"});
+            if(updatedPost){
+                setIsActive("inactive")
+            }
+        } else {
+            const updatedPost = await service.updatePost(slug, {...post, status: "active"});
+            if(updatedPost){
+                setIsActive("active")
+            }
+        }
+    }
 
     return post ? (
         <div className="py-8">
@@ -61,15 +66,26 @@ function Post() {
                 </div>
 
                 {isAuthor && (
-                    <div className="mt-12 mr-5 absolute right-10 top-12">
-                        <Link to={`/edit-post/${post.$id}`}>
-                            <Button bgColor="bg-green-600" className="mr-3 hover:bg-green-500">
-                                Edit
+                    <div>
+                        <div className="mt-12 mr-5 absolute left-10 top-12">
+                            <Button onClick={handleActive}
+                                bgColor={isActive==='active' ? 'bg-white' : 'bg-blue-600'} 
+                                textColor={isActive==='active' ? 'text-blue-600' : 'text-white'}
+                                className={`mr-3 px-4 py-3 hover:bg-blue-400 border-2 border-black`}>
+                                    {isActive==='active' && 'Set as inactive'}
+                                    {isActive==='inactive' && 'Set as active'}
                             </Button>
-                        </Link>
-                        <Button bgColor="bg-red-600" className="hover:bg-red-500" onClick={deletePost}>
-                            Delete
-                        </Button>
+                        </div>
+                        <div className="mt-12 mr-5 absolute right-10 top-12">
+                            <Link to={`/edit-post/${post.$id}`}>
+                                <Button bgColor="bg-green-600" className="mr-3 hover:bg-green-500">
+                                    Edit
+                                </Button>
+                            </Link>
+                            <Button bgColor="bg-red-600" className="hover:bg-red-500" onClick={deletePost}>
+                                Delete
+                            </Button>
+                        </div>
                     </div>
                 )}
 
